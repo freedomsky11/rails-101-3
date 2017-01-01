daac28a97af036ddbe3654e16f3be9e0e620cf7b
freedomsky <hanwei_1989@126.com>
2017-01-02 00:23:21 +0800

    implement edit/delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,17 @@
 class PostsController < ApplicationController
-  before_filter :authenticate_user!, :only => [:new, :create]
+  before_filter :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy ]
 
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
+  end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if current_user != @post.user
+      redirect_to root_path, alert: "You have no mission. "
+    end
   end
 
   def create
@@ -16,6 +24,29 @@ class PostsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if current_user != @post.user
+      redirect_to root_path, alert: "You have no mission"
+    end
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if current_user != @post.user
+      redirect_to root_path, alert: "You have no mission"
+    end
+    @post.destroy
+    redirect_to account_posts_path, alert: "Post deleded"
   end
 
   private
